f532fa9ebbe5b74d0a1bba5f165825220753c756
Filip Kašpar <filous.k@gmail.com>
2025-12-03 23:17:28 +0100

    day02: Yayy, we got valid IDs

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,57 +1,78 @@
-import java.lang.Math.pow
 import java.lang.invoke.MethodHandles
 
 fun main() {
     val folderName = MethodHandles.lookup().lookupClass().simpleName.removeSuffix("Kt")
 
-    fun part1(filename: String): Int {
+    fun part1(filename: String): Long {
         val input = readInput(filename)
         val ranges = input[0].split(",")
-        var invalidIds = 0
+        var invalidIds : Long = 0
 
-        // First Idea of faster computing, but it's taking too much time :)
         for (range in ranges) {
             val splitRange = range.split("-")
-            var curValue = splitRange[0].toInt()
-            val endValue = splitRange[1].toInt()
+            val curValue = splitRange[0].toLong()
+            val endValue = splitRange[1].toLong()
 
-            while (true) {
-                val curValueLength = curValue.toString().length
-
-                if (curValue > endValue) break
+            for (i in curValue..endValue) {
+                val iString = i.toString()
+                val curValueLength = iString.length
                 if (curValueLength % 2 == 1) {
-                    curValue = pow(10.toDouble(), curValueLength.toDouble()).toInt()
                     continue
                 }
 
-                val sequence = curValue.toString()
-
-                var first = sequence.substring(0, curValueLength / 2).toInt()
-
-                if ((first >= sequence.substring(curValueLength / 2, sequence.length).toInt()) &&
-                    ((sequence.substring(0, curValueLength / 2) + sequence.substring(0, curValueLength / 2)).toInt() <= endValue)) {
-                    invalidIds += (sequence.substring(0, curValueLength / 2) + sequence.substring(0, curValueLength / 2)).toInt()
+                if (iString.substring(0, iString.length / 2) == iString.substring(iString.length / 2, iString.length)) {
+                    invalidIds += i
                 }
-
-                curValue = (first + 1) * pow(10.toDouble(), sequence.substring(0, curValueLength / 2).length.toDouble()).toInt()
             }
-
-            // For sure can loop through every single number in the range
-
-            print(invalidIds)
-
         }
 
-        return 0
+        return invalidIds
     }
 
-    fun part2(filename: String): Int {
-        return 0
+    fun part2(filename: String): Long {
+        val input = readInput(filename)
+        val ranges = input[0].split(",")
+        var invalidIds : Long = 0
+
+        for (range in ranges) {
+            val splitRange = range.split("-")
+            val curValue = splitRange[0].toLong()
+            val endValue = splitRange[1].toLong()
+
+            for (number in curValue..endValue) {
+                val numberString = number.toString()
+
+                for (lastDigit in 1..numberString.length) {
+                    val sequence = numberString.substring(0, lastDigit)
+
+                    if (numberString.length % sequence.length != 0) continue
+                    if (sequence.length > numberString.substring(0, numberString.length / 2).length) continue
+
+                    var invalid = true
+                    val chunks = numberString.chunked(sequence.length).toMutableList()
+                    chunks.removeFirst()
+                    for (chunk in chunks) {
+                        if (chunk != sequence) {
+                            invalid = false
+                            break
+                        }
+                    }
+
+                    if (invalid) {
+                        invalidIds += numberString.toLong()
+                        break
+                    }
+                }
+            }
+        }
+
+        return invalidIds
     }
 
-    // Public test
-//     assert(part1("Day02/test") == 1227775554)
+    // Public tests
+    require(part1("$folderName/test") == 1227775554.toLong())
+    require(part2("$folderName/test") == 4174379265.toLong())
 
-    val result = part1("$folderName/test")
+    val result = part2("$folderName/input")
     print(result)
 }
